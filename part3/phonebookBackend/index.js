1cfaa2dc5faa49e5d80e181497135e269c851a4a
mahikgot <mahikgot@gmail.com>
2021-09-19 13:06:51 +0800

    finish phonebookFullStack 3.18

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -4,16 +4,19 @@ const morgan = require('morgan')
 const cors = require('cors')
 const Person = require('./models/note')
 
-const isValid = async (req) => {
+const isValid = async (req, meth) => {
     const body = req.body
     const result = await Person.find({name: body.name})
 
-    if (result.length > 0)
-        return valid = {validity: false, error: 'name must be unique'}
+    if (meth === 'post') {
+        if (result.length > 0)
+            return {validity: false, name: 'name must be unique'}
+    }
 
-    if (body.name === (undefined || '') || body.number === (undefined || '') )
-        return valid = {validity: false, error: 'body must have name and number'}
-    return valid = {validity: true, error: ''}
+    if ((body.name === undefined || body.name === '') || (body.number === undefined || body.number === ''))
+        return {validity: false, name: 'body must have name and number'}
+
+    return {validity: true, name: ''}
 }
 
 morgan.token('body', (req, res) => {
@@ -28,46 +31,53 @@ app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-app.get('/info', (req, res) => {
+app.get('/info', (req, res, next) => {
     const toSendFirst = `<p>Phonebook has info for ${Person.countDocuments({})} people<p>`
     const toSendSecond = `<p>${Date()}</p>`
     res.send(toSendFirst + toSendSecond)
 })
 
-app.get('/api/persons', (req, res) =>
+app.get('/api/persons', (req, res, next) =>
     Person.find({})
         .then(result => res.json(result))
+        .catch(error => next(error))
 )
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
     const id = req.params.id
     Person.find({_id: id})
         .then(result => {
-            console.log(result)
             if (result[0] === undefined) {
                 res.status(404).end()
             }
             else
                 res.json(result[0])
         })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
     const id = req.params.id
     Person.deleteOne({_id: id})
         .then(() => {
             res.status(204).end()
         })
+        .catch(error => next(error))
 })
 
-app.put('/api/persons/:id', (req, res) => {
+app.put('/api/persons/:id', async (req, res, next) => {
+    const isOk = await isValid(req, 'put')
     const id = req.params.id
-    Person.findOneAndReplace({_id: id}, req.body, {new: true})
-        .then(result => res.json(result))
+    if (isOk.validity)
+        Person.findOneAndReplace({_id: id}, req.body, {new: true})
+            .then(result => res.json(result))
+            .catch(error => next(error))
+    else
+        next(isOk)
 })
 
-app.post('/api/persons', async (req, res) => {
-    const isOk = await isValid(req)
+app.post('/api/persons', async (req, res, next) => {
+    const isOk = await isValid(req, 'post')
     if (isOk.validity) {
         const person = new Person({
             name: req.body.name,
@@ -76,12 +86,18 @@ app.post('/api/persons', async (req, res) => {
         person.save()
             .then(result => res.json(result))
     }
-    else {
-        console.log('boboka')
-        res.status(400).json({error: isOk.error})
-    }
+    else
+        next(isOk)
 })
 
+const errorHandler = (err, req, res, next) => {
+    if (err.name === 'CastError')
+        res.status(400).send({error: 'malformatted id'})
+    else if ((err.name === 'name must be unique') || (err.name === 'body must have name and number'))
+        res.status(400).send({error: err.name})
+    next(err)
+}
 
+app.use(errorHandler)
 const PORT = process.env.PORT || 3001
 app.listen(PORT)
