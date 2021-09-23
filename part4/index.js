9c4ec610b54a3fa35225a729442ac85150d41554
mahikgot <mahikgot@gmail.com>
2021-09-23 14:06:11 +0800

    blogList add express.json() middleware

--- part4/index.js
+++ part4/index.js
@@ -6,6 +6,7 @@ const blogListRouter = require('./controllers/blogs');
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 app.use(morgan('tiny'));
 
 app.use('/api/blogs', blogListRouter);
