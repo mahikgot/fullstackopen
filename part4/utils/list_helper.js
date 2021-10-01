3a821d4c6b40d9f9aca173f1c0fc9bcabe747aaf
mahikgot <mahikgot@gmail.com>
2021-10-01 12:50:12 +0800

    blogList install jest and create list_helper test 4.4

---
+++ part4/utils/list_helper.js
@@ -0,0 +1,25 @@
+const totalLikes = (blogs) => {
+  const reducer = (sum, prev) => sum + prev.likes;
+  return blogs.reduce(reducer, 0);
+};
+
+const favoriteBlog = (blogs) => {
+  const reducer = (highest, current) => {
+    if (highest < current.likes)
+      return current.likes;
+    return highest;
+  }
+  const highestLikes = blogs.reduce(reducer, 0);
+  const favorite = blogs.find(blog => blog.likes === highestLikes);
+  const returnee = {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes
+  };
+  return returnee;
+};
+
+const mostBlogs = (blogs) => {
+  const names = blogs.map(({ author }) => author)
+};
+module.exports = { totalLikes, favoriteBlog };
