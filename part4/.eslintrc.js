1e236d6ab4eb2d013cdec3868eb7f4fd0a236f65
Mark Guiang <mahikgot@gmail.com>
2021-10-16 00:39:26 +0800

    blogList add eslint config file

---
+++ part4/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  env: {
+    jest: true,
+    node: true,
+  },
+  extends: 'airbnb-base',
+  rules: {
+    'linebreak-style': ['error', 'windows'],
+    'no-underscore-dangle': ['error', { allow: ['__v', '_id'] }],
+  },
+};
