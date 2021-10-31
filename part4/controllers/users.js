6756d50ea9a14c6c42132d5660aa327ee403cb4e
unknown <mahikgot@gmail.com>
2021-10-31 10:37:55 +0800

    add user login checks

--- part4/controllers/users.js
+++ part4/controllers/users.js
@@ -7,6 +7,17 @@ const userRouter = express.Router();
 userRouter.post('/', async (req, res) => {
   const { body } = req;
 
+  if (!body.username || !body.password) {
+    res.status(400).json({ error: 'username and password required' });
+  } else if ((body.username.length < 3) || (body.password.length < 3)) {
+    res.status(400).json({ error: 'username and password must be at least 3 chars long' });
+  }
+
+  const temp = await User.find({ username: body.username });
+  if (temp.length >= 1) {
+    res.status(400).json({ error: 'username must be unique' });
+  }
+
   const passwordHash = await bcrypt.hash(body.password, 10);
   const userObject = {
     username: body.username,
